324c6a09dcd4cc103466d3595e01b2fda507ed0b
Max <mxbely98@gmail.com>
2025-01-10 15:42:04 +0200

    implemet solid principles

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import json
 import xml.etree.ElementTree as ET
 
@@ -7,46 +8,97 @@ class Book:
         self.title = title
         self.content = content
 
-    def display(self, display_type: str) -> None:
-        if display_type == "console":
-            print(self.content)
-        elif display_type == "reverse":
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown display type: {display_type}")
 
-    def print_book(self, print_type: str) -> None:
-        if print_type == "console":
-            print(f"Printing the book: {self.title}...")
-            print(self.content)
-        elif print_type == "reverse":
-            print(f"Printing the book in reverse: {self.title}...")
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown print type: {print_type}")
+class Display(ABC):
+    @abstractmethod
+    def display(self, book: Book) -> None:
+        pass
 
-    def serialize(self, serialize_type: str) -> str:
-        if serialize_type == "json":
-            return json.dumps({"title": self.title, "content": self.content})
-        elif serialize_type == "xml":
-            root = ET.Element("book")
-            title = ET.SubElement(root, "title")
-            title.text = self.title
-            content = ET.SubElement(root, "content")
-            content.text = self.content
-            return ET.tostring(root, encoding="unicode")
-        else:
-            raise ValueError(f"Unknown serialize type: {serialize_type}")
+
+class DisplayBookConsole(Display):
+    def display(self, book: Book) -> None:
+        print(book.content)
+
+
+class DisplayBookReverse(Display):
+    def display(self, book: Book) -> None:
+        print(book.content[::-1])
+
+
+class Print(ABC):
+    @abstractmethod
+    def print_book(self, book: Book) -> None:
+        pass
+
+
+class PrintBookConsole(Print):
+    def print_book(self, book: Book) -> None:
+        print(f"Printing the book: {book.title}...")
+        print(book.content)
+
+
+class PrintBookReverse(Print):
+    def print_book(self, book: Book) -> None:
+        print(f"Printing the book in reverse: {book.title}...")
+        print(book.content[::-1])
+
+
+class SerializeBook(ABC):
+    @abstractmethod
+    def serialize(self, book: Book) -> str:
+        pass
+
+
+class SerializeBookJson(SerializeBook):
+    def serialize(self, book) -> str:
+        return json.dumps({"title": book.title, "content": book.content})
+
+
+class SerializeBookXml(SerializeBook):
+    def serialize(self, book) -> str:
+        root = ET.Element("book")
+        title = ET.SubElement(root, "title")
+        title.text = book.title
+        content = ET.SubElement(root, "content")
+        content.text = book.content
+        return ET.tostring(root, encoding="unicode")
+
+
+def get_printer(cmd):
+    commands = {
+        "console": PrintBookConsole(),
+        "reverse": PrintBookReverse(),
+    }
+    return commands[cmd]
+
+
+def get_display(cmd):
+    commands = {
+        "console": DisplayBookConsole(),
+        "reverse": DisplayBookReverse(),
+    }
+    return commands[cmd]
+
+
+def get_serializer(cmd):
+    commands = {
+        "json": SerializeBookJson(),
+        "xml": SerializeBookXml(),
+    }
+    return commands[cmd]
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
         if cmd == "display":
-            book.display(method_type)
+            display = get_display(method_type)
+            display.display(book)
         elif cmd == "print":
-            book.print_book(method_type)
+            printer = get_printer(method_type)
+            printer.print_book(book)
         elif cmd == "serialize":
-            return book.serialize(method_type)
+            serializer = get_serializer(method_type)
+            return serializer.serialize(book)
 
 
 if __name__ == "__main__":
